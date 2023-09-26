a9706cb23e611c5757093ec5d9980c85fe19a408
Nicolas Pierrot <nicolas@archipels.io>
2023-09-26 21:49:33 +0200

    feat: add error handling

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,27 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, _ready: Ready) {
-        let wakeup_channel_id = env::var("WAKEUP_CHANNEL_ID").expect("wakeup channel id");
-        let wakeup_channel_id = wakeup_channel_id.parse::<u64>().expect("parse u64");
-        let channel = ctx
-            .http
-            .get_channel(wakeup_channel_id)
-            .await
-            .expect("Error getting channel");
-
-        let selim_id = env::var("SELIM_ID").expect("selim id");
-        let content = format!("T'as vu je suis codé en rust ! <@{}>", selim_id);
-        channel
-            .id()
-            .send_message(&ctx.http, |m| {
-                m.content(content);
-                m
-            })
-            .await
-            .expect("Error sending message");
+        if let (Ok(wakeup_channel_id), Ok(selim_id)) =
+            (env::var("WAKEUP_CHANNEL_ID"), env::var("SELIM_ID"))
+        {
+            if let (Ok(wakeup_channel_id), Ok(selim_id)) =
+                (wakeup_channel_id.parse::<u64>(), selim_id.parse::<u64>())
+            {
+                if let Ok(channel) = ctx.http.get_channel(wakeup_channel_id).await {
+                    let content = format!("T'as vu je suis codé en rust ! <@{}>", selim_id);
+                    if let Err(err) = channel
+                        .id()
+                        .send_message(&ctx.http, |m| {
+                            m.content(content);
+                            m
+                        })
+                        .await
+                    {
+                        eprintln!("Error sending message: {:?}", err);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -46,18 +49,19 @@ async fn main() {
         .configure(|c| c.prefix("*")) // set the bot's prefix to "*"
         .group(&GENERAL_GROUP);
 
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
+    if let Ok(token) = env::var("DISCORD_TOKEN") {
+        let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+        let mut client = Client::builder(token, intents)
+            .event_handler(Handler)
+            .framework(framework)
+            .await
+            .expect("Error creating client");
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+        if let Err(why) = client.start().await {
+            eprintln!("An error occurred while running the client: {:?}", why);
+        }
+    } else {
+        eprintln!("DISCORD_TOKEN not found in environment variables");
     }
 }
 
@@ -69,9 +73,12 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     let end_time = message.timestamp.timestamp();
     let latency = end_time - start_time;
 
-    message
+    if let Err(err) = message
         .edit(ctx, |m| m.content(format!("Pong! Latency: {}ms", latency)))
-        .await?;
+        .await
+    {
+        eprintln!("Error editing message: {:?}", err);
+    }
 
     Ok(())
 }
@@ -88,11 +95,13 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
         .field("*github", "Renvoie le lien du github", false)
         .field("*rust", "Renvoie le lien du rust", false);
 
-    let cloned_embed = embed.clone();
-
-    msg.channel_id
-        .send_message(&ctx.http, |m| m.set_embed(cloned_embed))
-        .await?;
+    if let Err(err) = msg
+        .channel_id
+        .send_message(&ctx.http, |m| m.set_embed(embed.clone()))
+        .await
+    {
+        eprintln!("Error sending help message: {:?}", err);
+    }
 
     Ok(())
 }
@@ -112,25 +121,31 @@ async fn infos(ctx: &Context, msg: &Message) -> CommandResult {
         .url("https://github.com/ncls-p/IABD2.RS")
         .image("https://cdn.discordapp.com/attachments/1028352036049277060/1156246329736044676/download.png?ex=651445cf&is=6512f44f&hm=19e23e3d7ec03497772c97daccaa35cbb978df8e4c104414c0c2e63c547091f0&");
 
-    let cloned_embed = embed.clone();
-
-    msg.channel_id
-        .send_message(&ctx.http, |m| m.set_embed(cloned_embed))
-        .await?;
+    if let Err(err) = msg
+        .channel_id
+        .send_message(&ctx.http, |m| m.set_embed(embed.clone()))
+        .await
+    {
+        eprintln!("Error sending info message: {:?}", err);
+    }
 
     Ok(())
 }
 
 #[command]
 async fn github(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "https://github.com/ncls-p/IABD2.RS").await?;
+    if let Err(err) = msg.reply(ctx, "https://github.com/ncls-p/IABD2.RS").await {
+        eprintln!("Error sending github link: {:?}", err);
+    }
 
     Ok(())
 }
 
 #[command]
 async fn rust(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "https://www.rust-lang.org/").await?;
+    if let Err(err) = msg.reply(ctx, "https://www.rust-lang.org/").await {
+        eprintln!("Error sending rust link: {:?}", err);
+    }
 
     Ok(())
 }
